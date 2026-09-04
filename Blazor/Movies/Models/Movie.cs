8ff398f5022b4146fc1933236f06e16bccab8a8c
Admin <Admin@DESKTOP-8URQBFL>
2026-09-04 23:58:20 +0500

    Movies commit

---
+++ Blazor/Movies/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Movies.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateOnly ReleaseDate { get; set; }
+        public string Genre { get; set; }
+
+    }
+}
