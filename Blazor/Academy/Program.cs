b5035d64a2f919b34c1c122642e23a56b9b1a66d
Admin <Admin@DESKTOP-8URQBFL>
2026-09-11 00:52:01 +0500

    Import DONE

---
+++ Blazor/Academy/Program.cs
@@ -0,0 +1,39 @@
+using Academy.Components;
+using Microsoft.EntityFrameworkCore;
+using System.Net.NetworkInformation;
+using Academy.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("PV_521_ImportContext") ?? throw new InvalidOperationException("Connection string 'PV_521_ImportContext' not found.");
+
+builder.Services.AddDbContextFactory<PV_521_ImportContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+    app.UseMigrationsEndPoint();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
