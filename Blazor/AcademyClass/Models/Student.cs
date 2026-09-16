0c85b684d53dd37826b7e1e265cd64eb3006ce01
Admin <Admin@DESKTOP-8URQBFL>
2026-09-16 22:27:15 +0500

    Lesson44 TODO scaffold Student

---
+++ Blazor/AcademyClass/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AcademyClass.Models
+{
+    public class Student : Human
+    {
+        [Key]
+        public int stud_id { get; set; }
+
+        [Required]
+        public int group {  get; set; }
+    }
+}
